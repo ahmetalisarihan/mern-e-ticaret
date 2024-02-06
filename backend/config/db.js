f6204bcccd04b88b8a24b60b8dacc256d0786ec6
ahmetalisarihan <105447763+ahmetalisarihan@users.noreply.github.com>
2024-02-06 23:55:11 +0300

    Add folders

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const db = () => {
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('Database connected');
+    }).catch(err => console.log(err));
+}
+
+module.exports = db;
