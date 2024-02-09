e01608de558f94a448f89ed38c99bd34104c57d6
ahmetalisarihan <105447763+ahmetalisarihan@users.noreply.github.com>
2024-02-09 23:51:52 +0300

    Add Review

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
 const ProductFilter = require('../utils/productFilter');
+const cloudinary = require('cloudinary').v2;
 
 const allProducts = async (req, res) => {
     const resultPerPage = 10;
@@ -19,27 +20,112 @@ const detailProducts = async (req, res) => {
     res.status(200).json({ product });
 }
 //Admin
-const createProduct = async (req, res) => {
+const createProduct = async (req, res, next) => {
+    let images = [];
+    if (typeof req.body.images==='string') {
+        images.push(req.body.images);
+       
+    }else{
+        images = req.body.images;
+    }
+
+    let allImages = [];
+    for (let i = 0; i < images.length; i++) {
+        const result = await cloudinary.uploader.upload(images[i], {
+            folder: 'products'
+        });
+        allImages.push({
+            public_id: result.public_id,
+            url: result.secure_url
+        });
+    }
+    req.body.images = allImages;
+
     const product = await Product.create(req.body);
 
     res.status(201).json({ product });
 }
 
-const deleteProduct = async (req, res) => {
+const deleteProduct = async (req, res, next) => {
     const product = await Product.findById(req.params.id);
 
-    product.remove();
+    for (let i = 0; i < product.images.length; i++) {
+        await cloudinary.uploader.destroy(product.images[i].public_id);
+    }
+
+    await product.remove();
     res.status(200).json({ 
         message: 'Product deleted successfully'
      });
 }
-const updateProduct = async (req, res) => {
+const updateProduct = async (req, res, next) => {
     const product = await Product.findById(req.params.id);
 
-    prodduct = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    let images = [];
+    if (typeof req.body.images==='string') {
+        images.push(req.body.images);
+       
+    }else{
+        images = req.body.images;
+    }
+
+    if(images !== undefined){
+        for (let i = 0; i < product.images.length; i++) {
+            await cloudinary.uploader.destroy(product.images[i].public_id);
+        }
+    }
+
+    let allImages = [];
+    for (let i = 0; i < images.length; i++) {
+        const result = await cloudinary.uploader.upload(images[i], {
+            folder: 'products'
+        });
+        allImages.push({
+            public_id: result.public_id,
+            url: result.secure_url
+        });
+    }
+    req.body.images = allImages;
+
+    prodduct = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true , runValidators: true})
     res.status(200).json({ 
         product
      });
 }
 
-module.exports = { allProducts, detailProducts, createProduct, deleteProduct, updateProduct};
+const createReview = async (req, res, next) => {
+    const { rating, comment, productId } = req.body;
+
+    const review = {
+        user: req.user._id,
+        name: req.user.name,
+        rating: Number(rating),
+        comment,
+    }
+
+    const product = await Product.findById(productId);
+
+    const isReviewed = product.reviews.find(
+        r => r.name === review.name
+    );
+
+    if (isReviewed) {
+        product.reviews.forEach(review => {
+            if (review.name === isReviewed.name) {
+                review.comment = comment;
+                review.rating = rating;
+            }
+        });
+    } else {
+        product.reviews.push(review);
+        product.numOfReviews = product.reviews.length;
+    }
+
+    product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+
+    await product.save({ validateBeforeSave: false });
+
+    res.status(200).json({ message: 'Review added' });
+}
+
+module.exports = { allProducts, detailProducts, createProduct, deleteProduct, updateProduct, createReview};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { allProducts, detailProducts, createProduct, deleteProduct, updateProduct} = require('../controllers/product');
+const { allProducts, detailProducts, createProduct, deleteProduct, updateProduct, createReview} = require('../controllers/product');
 
 const router = express.Router();
 
 router.get('/products', allProducts);
 router.get('/product/:id', detailProducts);
 router.post('/product/new', createProduct);
+router.post('/product/newReview', createReview);
 router.delete('/product/:id', deleteProduct);
 router.put('/product/:id', updateProduct);
 
